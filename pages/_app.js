860032f2641e9161e61f7628acc617f01f32e941
Amine Hamoudi <aminehamoudi92@gmail.com>
2021-03-22 14:28:37 +0100

    Premiere version STABLE

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,24 @@
 import '../styles/globals.css'
 
+// Importing the Bootstrap CSS
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+// pick a date util library
+import DateFnsUtils from '@date-io/date-fns';
+import { MuiPickersUtilsProvider,} from '@material-ui/pickers';
+import React from "react";
+import frLocale from "date-fns/locale/fr";
+
+const localeMap = {
+  fr: frLocale,
+};
+
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+      <MuiPickersUtilsProvider utils={DateFnsUtils} locale={localeMap["fr"]}>
+        <Component {...pageProps} />
+      </MuiPickersUtilsProvider>
+  );
 }
 
 export default MyApp
